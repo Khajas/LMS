41622a78d911089df21b7b3f82e235ad95833ce5
shoaibmomin93 <shoaibmomin93@gmail.com>
2017-01-29 13:03:39 -0600

    Update lmsDb.sql

--- DBA/lmsDb.sql
+++ DBA/lmsDb.sql
@@ -4,7 +4,7 @@ create table institute(instituteId INT(4) NOT NULL AUTO_INCREMENT, instiName VAR
 
 create table branch(branchId INT(4) NOT NULL AUTO_INCREMENT, instituteBranchId VARCHAR(255),branchName VARCHAR(255), branchAddr1 VARCHAR(255), branchAddr2 VARCHAR(255), phoneNum INT(10), instituteId INT(4), PRIMARY KEY (branchId), FOREIGN KEY (instituteId) REFERENCES institute(instituteId) );
 
-create table employee(empId VARCHAR(255) NOT NULL , empName VARCHAR(255), empPassword VARCHAR(255), salary DOUBLE(12,2), joinDate Date, branchId INT(4), PRIMARY KEY(empId), FOREIGN KEY (branchId) REFERENCES branch(branchId) ); 
+create table employee(empId VARCHAR(255) NOT NULL , ueid VARCHAR(255), empName VARCHAR(255), empPassword VARCHAR(255), salary DOUBLE(12,2), joinDate Date, branchId INT(4), PRIMARY KEY(empId), FOREIGN KEY (branchId) REFERENCES branch(branchId) ); 
 
 create table class(standard VARCHAR(255) NOT NULL, section VARCHAR(255) NOT NULL, academicYear VARCHAR(255) NOT NULL, branchId INT(4), PRIMARY KEY(standard,section,academicYear), FOREIGN KEY (branchId) REFERENCES branch(branchId) );
 
@@ -16,13 +16,16 @@ create table teaching(empId VARCHAR(255) NOT NULL , teachingRole VARCHAR(255), e
 
 create table classCourse(standard VARCHAR(255) NOT NULL , section VARCHAR(255) NOT NULL,academicYear VARCHAR(255) NOT NULL,  courseId INT(4) NOT NULL, PRIMARY KEY(standard,section,academicYear,courseId), FOREIGN KEY (standard,section,academicYear) REFERENCES class(standard,section,academicYear) , FOREIGN KEY (courseId) REFERENCES course(courseId)  );
 
-create table course(courseId INT(4) NOT NULL AUTO_INCREMENT, courseName VARCHAR(255), syllabus VARCHAR(255), PRIMARY KEY(courseId));
+create table course(courseId INT(4) NOT NULL AUTO_INCREMENT,standard VARCHAR(255) NOT NULL, section VARCHAR(255) NOT NULL, academicYear VARCHAR(255) NOT NULL, courseName VARCHAR(255), syllabus VARCHAR(255), PRIMARY KEY(courseId,standard,section,academicYear), FOREIGN KEY (standard, section, academicYear) REFERENCES class(standard, section, academicYear));
 
-create table exam(examId INT(4) NOT NULL AUTO_INCREMENT, examName VARCHAR(255), examDate DATE, courseId INT(4), PRIMARY KEY(examId,examDate),  FOREIGN KEY (courseId) REFERENCES course(courseId) );
+create table exam(examId INT(4) NOT NULL AUTO_INCREMENT,standard VARCHAR(255) NOT NULL, section VARCHAR(255) NOT NULL, academicYear VARCHAR(255) NOT NULL, examName VARCHAR(255), examDate DATE, courseId INT(4), PRIMARY KEY(examId,examDate,courseId,standard,section,academicYear),  FOREIGN KEY (courseId,standard,section,academicYear) REFERENCES course(courseId,standard,section,academicYear) );
 
 create table studentExamGrade(sId VARCHAR(255) NOT NULL , examId INT(4) NOT NULL, examDate Date NOT NULL, grade VARCHAR(255), PRIMARY KEY(sId, examId, examDate), FOREIGN KEY (examId,examDate) REFERENCES exam(examId,examDate) , FOREIGN KEY (sId) REFERENCES student(sId)  );
 
-create table teaches(empId VARCHAR(255) NOT NULL , courseId INT(4) NOT NULL, PRIMARY KEY(empId, courseId), FOREIGN KEY (empId) REFERENCES employee(empId) , FOREIGN KEY (courseId) REFERENCES course(courseId)  );
+create table teaches(empId VARCHAR(255) NOT NULL , courseId INT(4) NOT NULL,standard VARCHAR(255) NOT NULL, section VARCHAR(255) NOT NULL, academicYear VARCHAR(255) NOT NULL, PRIMARY KEY(empId, courseId,standard,section,academicYear), FOREIGN KEY (empId) REFERENCES employee(empId) , FOREIGN KEY (courseId,standard,section,academicYear) REFERENCES course(courseId,standard,section,academicYear)  );
+
+create table announcement(announceId INT(4) NOT NULL, announceTxt VARCHAR(255), announceDate DATE, empId VARCHAR(255), courseId INT(4), standard VARCHAR(255), section VARCHAR(255), academicYear VARCHAR(255), PRIMARY KEY(announceId), FOREIGN KEY (empID) REFERENCES employee(empId), FOREIGN KEY (courseId,standard,section,academicYear) REFERENCES course(courseId,standard,section,academicYear));
+
 SET foreign_key_checks =1;
 
 
